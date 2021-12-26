359fffb0ca850f23321e3c07f6130420fd460f7b
lilianeascosta <lilianecosta767@hotmail.com>
2021-12-26 16:54:01 -0300

    New project with two screens and also navigation between them

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:bytebank2/screens/contacts_list.dart';
+import 'package:bytebank2/screens/dashboard.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(ByteBank2());
+}
+
+class ByteBank2 extends StatelessWidget {
+  const ByteBank2({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.green[900],
+        accentColor: Colors.blueAccent[700],
+        buttonTheme: ButtonThemeData(
+          buttonColor: Colors.blueAccent[700],
+          textTheme: ButtonTextTheme.primary,
+        ),
+      ),
+      home: Dashboard(),
+    );
+  }
+}
